82c158d1e7491790f01333cd654777ad8d5d789c
VitorinoAlves <vitorino.17alexandre@gmail.com>
2025-01-12 18:12:49 -0300

    Adicionando acesso a API

--- tests/e2e/leads.spec.js
+++ tests/e2e/leads.spec.js
@@ -14,11 +14,33 @@ test.beforeEach(async ({page}) => {
 })
 
 test("Deve cadastrar um lead na fila de espera", async ({ page }) => {
+  const leadName = faker.person.fullName();
+  const leadEmail = faker.internet.email();
   await landingPage.visit();
   await landingPage.openLeadModal();
-  await landingPage.submitLeadForm(faker.person.fullName(), faker.internet.email());
+  await landingPage.submitLeadForm(leadName, leadEmail);
 
   const toastText = 'Agradecemos por compartilhar seus dados conosco. Em breve, nossa equipe entrará em contato!';
+  await toast.haveText(toastText);
+});
+
+test("Não deve cadastrar quando o email já existe", async ({ page, request }) => {
+  const leadName = faker.person.fullName();
+  const leadEmail = faker.internet.email();
+
+  const nweLead = await request.post('http://localhost:3333/leads', {
+    data:{
+      name: leadName,
+      email: leadEmail
+    }
+  })
+  expect(nweLead.ok()).toBeTruthy();
+
+  await landingPage.visit();
+  await landingPage.openLeadModal();
+  await landingPage.submitLeadForm(leadName, leadEmail);
+
+  const toastText = 'O endereço de e-mail fornecido já está registrado em nossa fila de espera.';
   await toast.haveText(toastText);
 });
 
---
+++ tests/e2e/movies.spec.js
@@ -0,0 +1,9 @@
+const { test } = require('@playwright/test')
+
+test.beforeEach(({ page }) => {
+    
+})
+
+test('Deve cadastrar um novo filme', async ({ page }) => {
+
+})
